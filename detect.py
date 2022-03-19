c5d028007de4ed5bd37488e2a4763c4f23376170
Tanisha Banik <26tanishabanik@gmail.com>
2022-03-19 15:07:20 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -34,7 +34,7 @@ def predict(image,
     return preds.pandas().xyxy
 
 
-def plot_image( image, df, txtbox_h = 120, txtbox_w = 300, txt_size = 1, box_color = (0, 0, 255), box_width = 2):
+def plot_image( image, df, txtbox_h = 120, txtbox_w = 100, txt_size = 1, box_color = (0, 255, 255), box_width = 2):
     # if no object is detected in image        
     if df[0].empty:
         return image
@@ -72,7 +72,7 @@ def plot_image( image, df, txtbox_h = 120, txtbox_w = 300, txt_size = 1, box_col
                           (x1, y1 - 50),
                           cv2.FONT_HERSHEY_SIMPLEX, 
                           txt_size, 
-                          (255, 255, 255), 5)
+                          (255, 255, 255), 2)
         
     return img
 
