893fefef24cafd609aba1f72d4cb0efe7653d005
Tanisha Banik <26tanishabanik@gmail.com>
2022-03-19 15:22:15 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -62,7 +62,7 @@ def plot_image( image, df, txtbox_h = 50, txtbox_w = 200, txt_size = 1, box_colo
 
         # plot rectangle for text
         img = cv2.rectangle(img, 
-                            (x1, y1-txtbox_h), 
+                            (x1-50, y1-txtbox_h), 
                             (x1 + txtbox_w, y1-50), 
                             box_color, -1)
 
